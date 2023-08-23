d54040dfa0fad9ce74ce071bd1c7c77bc0e07191
Luc <wolf-361@hotmail.com>
2023-08-23 14:09:35 -0400

    init express project

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+const express = require('express');
+import {Express} from 'express';
+
+const app:Express = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
