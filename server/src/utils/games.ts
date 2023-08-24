b2c0d399c72754426ae83eefd8a51314f70c96eb
Quentin ANIERE <quentin.aniere@gmail.com>
2023-08-24 18:42:18 +0200

    Add game code generation

---
+++ server/src/utils/games.ts
@@ -0,0 +1,9 @@
+export function generateGameCode(): string {
+
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let result = '';
+    for (let i = 0; i < 4; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
