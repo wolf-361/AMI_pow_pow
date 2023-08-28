3fa9e2add6b64d58d44698d9b217d20e6795b5a1
Luc <wolf-361@hotmail.com>
2023-08-28 16:11:45 -0400

    init front

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { HostComponent } from './components/pages/host/host.component';
+import { PlayerComponent } from './components/pages/player/player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    HostComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
